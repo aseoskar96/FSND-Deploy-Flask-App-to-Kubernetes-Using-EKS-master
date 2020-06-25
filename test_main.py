52779d23b8106f54849bc9c00b2cb31671bfc538
Oskar Lennartsson <ase.oskar@gmail.com>
2020-06-25 17:21:23 +0200

    secret in testin for eniormentviable

--- test_main.py
+++ test_main.py
@@ -11,13 +11,11 @@ SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
-LOG = 'DEBUG'
 
 
 @pytest.fixture
 def client():
-    os.environ['JWT_SECRET'] = SECRET
-    os.environ['LOG_LEVEL'] = LOG
+    #os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
